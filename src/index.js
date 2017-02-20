327e88b11baba73e15c7efbf4d0097f252bac7d0
Andrew Morris <voltrevo@gmail.com>
2017-02-20 13:59:50 +1100

    Move PositionalAudioPlayer to file

--- src/index.js
+++ src/index.js
@@ -1,36 +1,6 @@
 'use strict';
 
-function PositionalAudioPlayer(stream, leftRightBalance = 0.5) {
-  const player = {};
-
-  const audioCtx = new AudioContext();
-  const source = audioCtx.createMediaStreamSource(stream);
-
-  const splitter = audioCtx.createChannelSplitter(2);
-  const merger = audioCtx.createChannelMerger(2);
-
-  source.connect(splitter);
-
-  const leftGainNode = audioCtx.createGain();
-  const rightGainNode = audioCtx.createGain();
-
-  player.setLeftRightBalance = (balance) => {
-    leftGainNode.gain.value = Math.cos(0.5 * Math.PI * balance);
-    rightGainNode.gain.value = Math.sin(0.5 * Math.PI * balance);
-  };
-
-  player.setLeftRightBalance(leftRightBalance);
-
-  splitter.connect(leftGainNode, 0);
-  splitter.connect(rightGainNode, 1);
-
-  leftGainNode.connect(merger, 0, 0);
-  rightGainNode.connect(merger, 0, 1);
-
-  merger.connect(audioCtx.destination);
-
-  return player;
-}
+const PositionalAudioPlayer = require('./PositionalAudioPlayer');
 
 window.PositionalAudioPlayer = PositionalAudioPlayer;
 
